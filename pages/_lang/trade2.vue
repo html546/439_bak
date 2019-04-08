131adcc1be0a6ca9552fe1b9be1825aa247683a5
chengjiang <15712859926@163.com>
2019-04-08 17:40:47 +0800

    439反馈修改888

--- pages/_lang/trade2.vue
+++ pages/_lang/trade2.vue
@@ -472,7 +472,7 @@ export default {
   },
   computed: {
     money() {
-      return (this.form1.num * 6.3 / this.form1.usdtprice).toFixed(2);
+      return this.form1.usdtprice;
     }
   },
   methods: {
