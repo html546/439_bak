6bf661d3d509cc62190cc9ce35ec100b7557ad71
html546 <1776787598@qq.com>
2019-04-25 14:53:56 +0800

    转账更新

--- pages/_lang/transfer/_id.vue
+++ pages/_lang/transfer/_id.vue
@@ -2,15 +2,9 @@
   <div>
     <div class="transfer_detail">
       <div class="form_box1">
-        <el-form
-          ref="form"
-          label-width="120px"
-        >
+        <el-form ref="form" label-width="120px">
           <el-form-item :label="$t('transfer.balance')">
-            <el-input
-              v-model="form.money1"
-              :readonly="true"
-            ></el-input>
+            <el-input v-model="form.money1" :readonly="true"></el-input>
           </el-form-item>
           <el-form-item :label="$t('transfer.amount')">
             <el-input v-model="form.money"></el-input>
@@ -30,67 +24,34 @@
               ></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item
-            :label="$t('transfer.number')"
-            v-if="!form.isme"
-          >
+          <el-form-item :label="$t('transfer.number')" v-if="!form.isme">
             <el-input v-model="form.username"></el-input>
           </el-form-item>
           <el-form-item :label="$t('transfer.mini')">
-            <el-input
-              v-model="form.minmoney"
-              :readonly="true"
-            ></el-input>
+            <el-input v-model="form.minmoney" :readonly="true"></el-input>
           </el-form-item>
           <el-form-item :label="$t('transfer.max')">
-            <el-input
-              v-model="form.maxmoney"
-              :readonly="true"
-            ></el-input>
+            <el-input v-model="form.maxmoney" :readonly="true"></el-input>
           </el-form-item>
           <el-form-item :label="$t('transfer.multiple')">
-            <el-input
-              v-model="form.multiple"
-              :readonly="true"
-            ></el-input>
+            <el-input v-model="form.multiple" :readonly="true"></el-input>
           </el-form-item>
           <el-form-item :label="$t('transfer.rate')">
-            <el-input
-              v-model="form.rate"
-              :readonly="true"
-            ></el-input>
+            <el-input v-model="form.rate" :readonly="true"></el-input>
           </el-form-item>
           <el-form-item :label="$t('transfer.charge')">
-            <el-input
-              v-model="form.fee"
-              :readonly="true"
-            ></el-input>
+            <el-input v-model="form.fee" :readonly="true"></el-input>
           </el-form-item>
-          <el-form-item
-            :label="$t('transfer.pass2')"
-            v-if="cashPass2 == 'true'"
-          >
-            <el-input
-              v-model="form.pass2"
-              type="password"
-            ></el-input>
+          <el-form-item :label="$t('transfer.pass2')" v-if="cashPass2 == 'true'">
+            <el-input v-model="form.pass2" type="password"></el-input>
           </el-form-item>
           <template v-if="cashcheckmess == 'true'">
             <el-form-item :label="$t('transfer.tel')">
               <el-input v-model="mobile"></el-input>
             </el-form-item>
             <el-form-item :label="$t('transfer.Captcha')">
-              <el-input
-                type="text"
-                v-model="verify_code"
-                style="width:70%;float:left;"
-              ></el-input>
-              <img
-                :src="imageUrl"
-                @click="getVerifyCode"
-                style="width:30%;height:40px;"
-                alt=""
-              >
+              <el-input type="text" v-model="verify_code" style="width:70%;float:left;"></el-input>
+              <img :src="imageUrl" @click="getVerifyCode" style="width:30%;height:40px;" alt>
             </el-form-item>
             <el-form-item :label="$t('transfer.code')">
               <el-input
@@ -98,8 +59,7 @@
                 v-model="mobile_code"
                 name="mobile_code"
                 style="width:70%;float:left;"
-              >
-              </el-input>
+              ></el-input>
               <el-button
                 type="text"
                 @click="getverify"
@@ -109,10 +69,7 @@
             </el-form-item>
           </template>
           <el-form-item>
-            <el-button
-              type="primary"
-              @click="handleSubmit"
-            >提交</el-button>
+            <el-button type="primary" @click="handleSubmit">提交</el-button>
           </el-form-item>
         </el-form>
       </div>
@@ -121,34 +78,34 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-  name: '',
+  name: "",
   data() {
     return {
       form: {
-        money1: '',
-        money: '',
-        type: '',
-        minmoney: '',
-        maxmoney: '',
-        multiple: '',
-        rate: '',
-        fee: '',
-        username: '',
+        money1: "",
+        money: "",
+        type: "",
+        minmoney: "",
+        maxmoney: "",
+        multiple: "",
+        rate: "",
+        fee: "",
+        username: "",
         isme: true,
-        pass2: '',
+        pass2: ""
       },
       transfers: [],
-      cashPass2: '',
-      cashcheckmess: '',
-      verify_code: '',
-      imageUrl: '',
-      mobile_code: '',
+      cashPass2: "",
+      cashcheckmess: "",
+      verify_code: "",
+      imageUrl: "",
+      mobile_code: "",
       disabled: false,
-      name: this.$t('friend.getcode'),
-      mobile: ''
-    }
+      name: this.$t("friend.getcode"),
+      mobile: ""
+    };
   },
   middleware: "auth",
   created() {
@@ -158,77 +115,102 @@ export default {
   methods: {
     onclose1() {
       setTimeout(() => {
-        this.$router.replace('/login');
+        this.$router.replace("/login");
       }, 3000);
     },
     getData() {
-      axios.post('/api/finance/transfer', {
-        userid: this.$store.state.message.userid,
-        sessionid: this.$store.state.message.sessionid,
-        type: this.$route.params.id
-      }).then(res => {
-        if (res.data.status == 0) {
-          this.$store.commit('clearMessage');
-          this.$message({
-            type: 'error',
-            message: res.data.msg,
-            onClose: this.onclose1()
-          })
-        }
-        console.log(res);
-        this.form.money1 = res.data.data.money;
-        this.transfers = res.data.data.transfers;
-        this.cashPass2 = res.data.data.cashPass2;
-        this.cashcheckmess = res.data.data.cashcheckmess;
-      }).catch(err => {
-        console.log(err);
-      })
+      axios
+        .post("/api/finance/transfer", {
+          userid: this.$store.state.message.userid,
+          sessionid: this.$store.state.message.sessionid,
+          type: this.$route.params.id
+        })
+        .then(res => {
+          if (res.data.status == 0) {
+            this.$store.commit("clearMessage");
+            this.$message({
+              type: "error",
+              message: res.data.msg,
+              onClose: this.onclose1()
+            });
+          }
+          console.log(res);
+          this.form.money1 = res.data.data.money;
+          this.transfers = res.data.data.transfers;
+          this.cashPass2 = res.data.data.cashPass2;
+          this.cashcheckmess = res.data.data.cashcheckmess;
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
     handleChange(val) {
-      this.transfers.forEach((item, index) => {
-        if (index == val) {
-          this.form.minmoney = item.minmoney;
-          this.form.maxmoney = item.maxmoney;
-          this.form.multiple = item.multiple;
-          this.form.rate = item.exchange_rate;
-          this.form.fee = item.servicecharge;
-          if (item.isme == '0') {
-            this.form.isme = false;
+      if (this.transfers.length) {
+        this.transfers.forEach((item, index) => {
+          if (index == val) {
+            this.form.minmoney = item.minmoney;
+            this.form.maxmoney = item.maxmoney;
+            this.form.multiple = item.multiple;
+            this.form.rate = item.exchange_rate;
+            this.form.fee = item.servicecharge;
+            if (item.isme == "0") {
+              this.form.isme = false;
+            }
+          }
+        });
+      } else {
+        console.log(val);
+        for (const key in this.transfers) {
+          if (key == val) {
+            this.form.minmoney = this.transfers[key].minmoney;
+            this.form.maxmoney = this.transfers[key].maxmoney;
+            this.form.multiple = this.transfers[key].multiple;
+            this.form.rate = this.transfers[key].exchange_rate;
+            this.form.fee = this.transfers[key].servicecharge;
+            if (this.transfers[key].isme == "0") {
+              this.form.isme = false;
+            }
           }
         }
-      })
+      }
     },
     getVerifyCode() {
-      axios.post('/api/login/getVerifyCode').then(res => {
-        this.imageUrl = res.data.image;
-        this.encrypt_code = res.data.encryptCode;
-      }).catch(err => {
-        console.log(err);
-      })
+      axios
+        .post("/api/login/getVerifyCode")
+        .then(res => {
+          this.imageUrl = res.data.image;
+          this.encrypt_code = res.data.encryptCode;
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
     getverify() {
-      axios.post('/api/login/verify', {
-        verify_code: this.verify_code,
-        encrypt_code: this.encrypt_code,
-        mobile_phone: this.mobile
-      }).then(res => {
-        if (res.data.status == 1) {
-          this.$message({
-            message: res.data.msg,
-            type: 'success',
-            showClose: true,
-            onClose: this.onclose()
-          })
-        } else {
-          this.$message({
-            message: res.data.msg,
-            type: 'danger',
-            showClose: true
-          })
-        }
-      }).catch(err => {
-        console.log(err);
-      })
+      axios
+        .post("/api/login/verify", {
+          verify_code: this.verify_code,
+          encrypt_code: this.encrypt_code,
+          mobile_phone: this.mobile
+        })
+        .then(res => {
+          if (res.data.status == 1) {
+            this.$message({
+              message: res.data.msg,
+              type: "success",
+              showClose: true,
+              onClose: this.onclose()
+            });
+          } else {
+            this.$message({
+              message: res.data.msg,
+              type: "danger",
+              showClose: true
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
     onclose() {
       let i = 60;
@@ -238,7 +220,7 @@ export default {
           this.name = `${i}s`;
           this.disabled = true;
         } else {
-          this.name = this.$t('register.retrieve');
+          this.name = this.$t("register.retrieve");
           this.disabled = false;
           clearInterval(timer);
         }
@@ -246,37 +228,40 @@ export default {
     },
     handleSubmit(event) {
       event.preventDefault();
-      axios.post('/api/finance/transfers', {
-        userid: this.$store.state.message.userid,
-        sessionid: this.$store.state.message.sessionid,
-        banktype: this.$route.params.id,
-        username: this.form.username,
-        money: this.form.money,
-        givekey: this.form.type,
-        transferPass2: this.form.pass2,
-        mobile_code: this.mobile_code,
-        mobile_phone: this.mobile
-      }).then(res => {
-        console.log(res);
-        if (res.data.status == 1) {
-          this.$message({
-            type: 'success',
-            message: res.data.msg,
-            showClose: true
-          })
-        } else {
-          this.$message({
-            type: 'error',
-            message: res.data.msg,
-            showClose: true
-          })
-        }
-      }).catch(err => {
-        console.log(err);
-      })
+      axios
+        .post("/api/finance/transfers", {
+          userid: this.$store.state.message.userid,
+          sessionid: this.$store.state.message.sessionid,
+          banktype: this.$route.params.id,
+          username: this.form.username,
+          money: this.form.money,
+          givekey: this.form.type,
+          transferPass2: this.form.pass2,
+          mobile_code: this.mobile_code,
+          mobile_phone: this.mobile
+        })
+        .then(res => {
+          console.log(res);
+          if (res.data.status == 1) {
+            this.$message({
+              type: "success",
+              message: res.data.msg,
+              showClose: true
+            });
+          } else {
+            this.$message({
+              type: "error",
+              message: res.data.msg,
+              showClose: true
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
-  },
-}
+  }
+};
 </script>
 
 <style >
